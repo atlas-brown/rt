b87f8d77fb3b40323bda23b969a77f2de19350fe
Infinite <infiniteloop654@gmail.com>
2024-10-11 14:51:24 -0400

    add gitignore

--- evaluation_pipelines/vaid/6.sh
+++
@@ -1,6 +0,0 @@
-cat $1 \
-	| grep -v '^#' \
-	| sed 's/^[^<]*<\([^>]*\)>/\1/' \
-	| grep '<.*>' | sed -e 's/[<>]/ /g' \
-	| awk '{if ($3 != "") { print $3" "$1 } else {print $2" "$1}}' \
-	| sort | uniq
