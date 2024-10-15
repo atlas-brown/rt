1d1a94c0e9121c9820a04c69fcdd604fbdc2f01c
Infinite <infiniteloop654@gmail.com>
2024-10-15 11:41:10 -0400

    update some comments

--- src/stream/command_signature.py
+++ src/stream/command_signature.py
@@ -32,61 +32,15 @@ class CommandSignature:
         return re.compile(f"^{regex_pattern}$")
 
     def matches_command(self, node: CommandInvocationInitial) -> bool:
-        # assert len(self.format) > 0
         assert isinstance(node, CommandInvocationInitial)
         # need to be more precise
         return self.command_name == node.cmd_name
-
-    # def extract_flags_and_args(self, node: CommandInvocationInitial) -> Tuple[List[str], Dict[str, List[str]]]:
-        # parsed_flags: List[str] = [] # [flag_name] e.g., ["-v", "-i"]
-        # parsed_args: Dict[List[str]] = {} # {arg_name: [arg_value]}, e.g., {"pattern: ["123"]"}
-
-        # index = 0
-        # arg_index = 0
-        # for element in self.format:
-        #     match element:
-        #         case "flag?":
-        #             if index < len(command_parts) and self.is_flag(command_parts[index]):
-        #                 parsed_flags.append(command_parts[index])
-        #                 flag_name = command_parts[index]
-        #                 flag = next((flag for flag in self.flags if flag['name'] == flag_name), None)
-        #                 if flag is not None and flag.get('argument', True):
-        #                     index += 2
-        #                 else:
-        #                     index += 1
-        #         case "arg":
-        #             if index < len(command_parts):
-        #                 parsed_args[self.args[arg_index]['name']] = [command_parts[index]]
-        #                 index += 1
-        #                 arg_index += 1
-        #             else:
-        #                 raise ValueError(f"Missing required argument for {self.command_name}.")
-        #         case "arg?":
-        #             if index < len(command_parts):
-        #                 parsed_args[self.args[arg_index]['name']] = [command_parts[index]]
-        #                 index += 1
-        #                 arg_index += 1
-        #             else:
-        #                 parsed_args[self.args[arg_index]['name']] = []
-        #         case "arg+":
-        #             parsed_args[self.args[arg_index]['name']] = []
-        #             while index < len(command_parts):
-        #                 parsed_args[self.args[arg_index]['name']].append(command_parts[index])
-        #                 index += 1
-        #             arg_index += 1
-        #         case _:
-        #             index += 1
-
-        # return parsed_flags, parsed_args
-
-    # def is_flag(self, arg: str) -> bool:
-    #     # need to be refined
-    #     return arg.startswith("-") and len(arg) > 1
 
     def determine_input_output_type(self, previous_output_type: RegularType, parsed_command_node: CommandInvocationInitial) -> Tuple[RegularType, RegularType]:
         assert isinstance(previous_output_type, RegularType)
         assert isinstance(parsed_command_node, CommandInvocationInitial)
 
+        # obtain the arguments {arg_name: arg_value} from the parsed command node, need to be more precise
         structured_args: Dict[str, Optional[str]] = {}
         for i, arg in enumerate(self.args):
             arg_name = arg['name']
@@ -95,26 +49,17 @@ class CommandSignature:
             else:
                 structured_args[arg_name] = None
 
-        # for arg in self.args:
-        #     assert isinstance(arg['name'], str)
-        #     arg_name = arg['name']
-        #     if arg_name in map(lambda operand: operand.name
-        #                        , parsed_command_node.operand_list):
-        #         structured_args[arg_name] = parsed_command_node.operand_list[0].name
-        #     else:
-        #         structured_args[arg_name] = None
-
+        # determine the output type based rules, parametric polymorphism supported, e.g., grep .*{pattern}.*&{actual_input_type} can be resolved
         for rule in self.rules:
             required_flags = set(rule['condition'].get('flags', []))
-            # flag_option_list is union type Union[Flag, Option], I only need the flag part, and it should be flag.get_name()
-            if required_flags.issubset(set(map(lambda flag: flag.get_name(), parsed_command_node.flag_option_list))):
+            if required_flags.issubset(set(map(lambda flag_option: flag_option.get_name(), parsed_command_node.flag_option_list))):
                 output_type_pattern: str = rule.get('output_type', self.default_output_type.pattern)
                 input_type_pattern: str = rule.get('input_type', self.default_input_type.pattern)
-                output_type_pattern = output_type_pattern.replace('{actual_input_type}', previous_output_type.pattern)
+                output_type_pattern = output_type_pattern.replace('{actual_input_type}', previous_output_type.pattern) # replace the placeholder with the actual input type
                 for arg in self.args:
                     arg_name = arg['name']
                     if arg_name in structured_args and structured_args[arg_name] is not None:
-                        output_type_pattern = output_type_pattern.replace(f'{{{arg_name}}}', structured_args[arg_name]) # replace the placeholder with the actual value, r.g., grep {pattern} to grep 123
+                        output_type_pattern = output_type_pattern.replace(f'{{{arg_name}}}', structured_args[arg_name]) # replace the placeholder with the actual value, e.g., grep {pattern} to grep 123
                 print(f"Input type: {input_type_pattern}, Output type: {output_type_pattern}")
                 return RegularType(input_type_pattern), RegularType(output_type_pattern)
 
