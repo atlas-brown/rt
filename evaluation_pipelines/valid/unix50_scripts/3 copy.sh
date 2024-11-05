6566a37c0a61650b521dd217849e4789f951ce39
Zekai Li <zekai.li.0028@gmail.com>
2024-11-05 12:37:51 -0500

    update annotation format

--- evaluation_pipelines/valid/unix50_scripts/3 copy.sh
+++ evaluation_pipelines/valid/unix50_scripts/3 copy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # 1.2: extract names and sort
-cat $1 "#.* .*" | head -n 2 | cut -d ' ' -f 2
+cat $1 "-->.* .*" | head -n 2 | cut -d ' ' -f 2
--- src/stream/command_signature.py
+++ src/stream/command_signature.py
@@ -34,7 +34,7 @@ class CommandSignature:
     def determine_output_type(self, previous_output_type: RegularType, parsed_command_node: CommandInvocationInitial) -> RegularType:
         # if user annotation is available, use it
         # otherwise, use inference
-        if len(parsed_command_node.operand_list) > 0 and parsed_command_node.operand_list[-1].name.startswith("#"):
+        if len(parsed_command_node.operand_list) > 0 and parsed_command_node.operand_list[-1].name.startswith("-->"):
             annotated_output_type = parsed_command_node.operand_list[-1].name[1:]
             return RegularType(annotated_output_type)
         else:
@@ -46,7 +46,7 @@ class CommandSignature:
         operand_list = parsed_command_node.operand_list.copy()
         if len(operand_list) == 0:
             return []
-        if operand_list[-1].name.startswith("#"):
+        if operand_list[-1].name.startswith("-->"):
             # ignore the user annotation
             operand_list.pop()
         if parsed_command_node.cmd_name == "xargs":
