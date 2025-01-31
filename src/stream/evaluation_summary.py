70d2a567a1e115e7ffc71973b37e9f776c2c4db7
Zekai Li <zekai.li.0028@gmail.com>
2025-01-31 11:12:13 -0500

    add full benchmark spreadsheets

--- src/stream/evaluation_summary.py
+++ src/stream/evaluation_summary.py
@@ -3,11 +3,13 @@ import json
 path = './evaluation_results/evaluation_results.json'
 outpath = './evaluation_results/results.csv'
 
-def split_id_into_name_and_collection(address):
+def convert_to_github_address(address):
     parts = address.split('/')
-    name = parts[-1]
     collection = '/'.join(parts[:-1])
-    return name, collection
+    if address.startswith('./'):
+        address = address[2:]
+    address = "https://github.com/brown-cs2952r/StreamTypes/blob/main/" + address
+    return address, collection
 
 def process_runtime_error(error):
     if error is None:
@@ -35,7 +37,7 @@ def results_to_summary_csv(json_path, out_path):
     with open(out_path, 'w') as f:
         f.write('Benchmark,Collection,Buggy?,Correct Result?,Time(s),RunTime Error,Category,Notes,Pipeline\n')
         for result in results:
-            name, collection = split_id_into_name_and_collection(result["address"])
+            name, collection = convert_to_github_address(result["address"])
             f.write(f'{name},{collection},{result["is buggy?"]},{result["warning signaled?"] == result["is buggy?"]},{result["evaluation_time"]},{process_runtime_error(result["tool runtime error"])},{process_category(result["category"])}, {result["notes"]},{process_content(result["content"])}\n')
 
 if __name__ == '__main__':
