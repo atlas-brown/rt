067e37ee4b1986f9f467a3c435ee22ca9f15c557
Zekai Li <zekai.li.0028@gmail.com>
2025-02-27 07:33:20 -0500

    add hole support in regex parser

--- src/stream/regex_parser.py
+++ src/stream/regex_parser.py
@@ -1,4 +1,9 @@
+from typing import Optional
 import z3
+import jpype.imports
+if not jpype.isJVMStarted():
+    jpype.startJVM(classpath=["jars/automaton.jar"])
+from dk.brics.automaton import RegExp, Automaton, BasicOperations, BasicAutomata # type: ignore
 
 class Node:
     pass
@@ -13,19 +18,19 @@ class Dot(Node):
     def __repr__(self):
         return "Dot()"
 
-class Concat(Node):
+class Concatenate(Node):
     def __init__(self, nodes: list[Node]):
         self.nodes = nodes
     def __repr__(self):
-        return f"Concat({self.nodes})"
+        return f"Concatenate({self.nodes})"
 
-class Quantifier(Node):
+class Repeat(Node):
     def __init__(self, node: Node, min_times: int, max_times: int | None):
         self.node = node
         self.min = min_times
         self.max = max_times
     def __repr__(self):
-        return f"Quantifier({self.node}, {self.min}, {self.max})"
+        return f"Repeat({self.node}, {self.min}, {self.max})"
 
 class Range(Node):
     def __init__(self, start: str, end: str):
@@ -60,12 +65,12 @@ class Complement(Node):
     def __repr__(self):
         return f"Complement({self.node})"
 
-class Alternation(Node):
+class Union(Node):
     def __init__(self, left: Node, right: Node):
         self.left = left
         self.right = right
     def __repr__(self):
-        return f"Alternation({self.left}, {self.right})"
+        return f"Union({self.left}, {self.right})"
 
 class StartAnchor(Node):
     def __repr__(self):
@@ -74,6 +79,12 @@ class StartAnchor(Node):
 class EndAnchor(Node):
     def __repr__(self):
         return "EndAnchor()"
+
+class Hole(Node):
+    def __init__(self, name: str):
+        self.name = name
+    def __repr__(self):
+        return f"Hole({self.name!r})"
 
 class RegexParser:
     def __init__(self, pattern, mode="compat"):
@@ -118,13 +129,13 @@ class RegexParser:
             while self.current() == '|':
                 self.consume('|')
                 right = self.parse_intersect_expr()
-                node = Alternation(node, right)
+                node = Union(node, right)
         else:
             while self.current() == '\\' and self.peek_next() == '|':
                 self.consume('\\')
                 self.consume('|')
                 right = self.parse_intersect_expr()
-                node = Alternation(node, right)
+                node = Union(node, right)
         return node
 
     def parse_intersect_expr(self):
@@ -150,7 +161,7 @@ class RegexParser:
             return Literal("")
         if len(nodes) == 1:
             return nodes[0]
-        return Concat(nodes)
+        return Concatenate(nodes)
     
     def _concat_terminator(self):
         cur = self.current()
@@ -171,14 +182,14 @@ class RegexParser:
                 if curr is not None and curr in ('*', '+', '?'):
                     op = self.consume()
                     if op == '*':
-                        node = Quantifier(node, 0, None)
+                        node = Repeat(node, 0, None)
                     elif op == '+':
-                        node = Quantifier(node, 1, None)
+                        node = Repeat(node, 1, None)
                     elif op == '?':
-                        node = Quantifier(node, 0, 1)
-                elif curr == '{':
+                        node = Repeat(node, 0, 1)
+                elif curr == '{' and self.peek_next() != '{':
                     min_val, max_val = self.parse_braced_quantifier(escaped=False)
-                    node = Quantifier(node, min_val, max_val)
+                    node = Repeat(node, min_val, max_val)
                 else:
                     break
             return node
@@ -187,17 +198,17 @@ class RegexParser:
                 curr = self.current()
                 if curr == '*':
                     self.consume('*')
-                    node = Quantifier(node, 0, None)
+                    node = Repeat(node, 0, None)
                 elif curr == '\\' and self.peek_next() in ['+', '?', '{']:
                     self.consume('\\')
                     op = self.consume()
                     if op == '+':
-                        node = Quantifier(node, 1, None)
+                        node = Repeat(node, 1, None)
                     elif op == '?':
-                        node = Quantifier(node, 0, 1)
+                        node = Repeat(node, 0, 1)
                     elif op == '{':
                         min_val, max_val = self.parse_braced_quantifier(escaped=True)
-                        node = Quantifier(node, min_val, max_val)
+                        node = Repeat(node, min_val, max_val)
                 else:
                     break
             return node
@@ -244,10 +255,28 @@ class RegexParser:
         else:
             return self.parse_primary()
 
+    def parse_hole(self):
+        self.consume('{')
+        if self.current() != '{':
+            self.error("Expected '{{' for hole")
+        self.consume('{')
+        name = ""
+        while self.current() is not None and not (self.current() == '}' and self.peek_next() == '}'):
+            name += self.consume()
+        if self.current() is None:
+            self.error("Unterminated hole, expected '}}'")
+        self.consume('}')
+        if self.current() != '}':
+            self.error("Expected '}}' to close hole")
+        self.consume('}')
+        return Hole(name)
+
     def parse_primary(self):
         curr = self.current()
         if curr is None:
             self.error("Unexpected end of expression")
+        if self.mode == "compat" and curr == '{' and self.peek_next() == '{':
+            return self.parse_hole()
         if self.mode in ("compat", "extended"):
             if curr == '(':
                 self.consume('(')
@@ -401,16 +430,18 @@ class RegexParser:
         return PosixClass(name)
 
 def get_prec(node):
-    if isinstance(node, Alternation):
+    if isinstance(node, Union):
         return 1
     elif isinstance(node, Intersection):
         return 2
-    elif isinstance(node, Concat):
+    elif isinstance(node, Concatenate):
         return 3
     elif isinstance(node, Complement):
         return 4
-    elif isinstance(node, Quantifier):
+    elif isinstance(node, Repeat):
         return 5
+    elif isinstance(node, Hole):
+        return 6
     else:
         return 6
 
@@ -434,9 +465,9 @@ def _ast_to_regex(node, parent_prec=0):
             s = escape_literal(node.char)
     elif isinstance(node, Dot):
         s = "."
-    elif isinstance(node, Concat):
+    elif isinstance(node, Concatenate):
         s = "".join(_ast_to_regex(child, get_prec(node)) for child in node.nodes)
-    elif isinstance(node, Quantifier):
+    elif isinstance(node, Repeat):
         base = _ast_to_regex(node.node, get_prec(node))
         if node.min == 0 and node.max is None:
             quant = "*"
@@ -471,7 +502,7 @@ def _ast_to_regex(node, parent_prec=0):
         s = left + "&" + right
     elif isinstance(node, Complement):
         s = "~" + _ast_to_regex(node.node, get_prec(node))
-    elif isinstance(node, Alternation):
+    elif isinstance(node, Union):
         left = _ast_to_regex(node.left, get_prec(node))
         right = _ast_to_regex(node.right, get_prec(node)+1)
         s = left + "|" + right
@@ -479,6 +510,8 @@ def _ast_to_regex(node, parent_prec=0):
         s = "^"
     elif isinstance(node, EndAnchor):
         s = "$"
+    elif isinstance(node, Hole):
+        s = f"{{{{{node.name}}}}}"
     else:
         s = ""
     if my_prec < parent_prec:
@@ -494,7 +527,7 @@ def ast_to_z3(node):
         return z3.Re(node.char)
     elif isinstance(node, Dot):
         return any_char
-    elif isinstance(node, Concat):
+    elif isinstance(node, Concatenate):
         children = [ast_to_z3(child) for child in node.nodes]
         children = [child for child in children if child != None]
         if len(children) == 0:
@@ -502,7 +535,7 @@ def ast_to_z3(node):
         if len(children) == 1:
             return children[0]
         return z3.Concat(children)
-    elif isinstance(node, Quantifier):
+    elif isinstance(node, Repeat):
         base = ast_to_z3(node.node)
         if node.min == 0 and node.max == 1:
             return z3.Option(base)
@@ -525,7 +558,7 @@ def ast_to_z3(node):
         return z3.Intersect(ast_to_z3(node.left), ast_to_z3(node.right))
     elif isinstance(node, Complement):
         return z3.Intersect(z3.Star(any_char), z3.Complement(ast_to_z3(node.node)))
-    elif isinstance(node, Alternation):
+    elif isinstance(node, Union):
         return z3.Union(ast_to_z3(node.left), ast_to_z3(node.right))
     elif isinstance(node, PosixClass):
         name = node.name
@@ -571,27 +604,115 @@ def ast_to_z3(node):
         return z3.Re("")
     elif isinstance(node, EndAnchor):
         return z3.Re("")
+    elif isinstance(node, Hole):
+        raise ValueError("Hole node is not supported in Z3 translation")
     else:
         raise ValueError(f"Unknown node type: {node}")
 
 def ast_to_regex(ast):
     return _ast_to_regex(ast, 0)
 
+
+def _ast_to_automaton(node: Node, hole_dict: Optional[dict[str, Automaton]] = None) -> Automaton:
+    if hole_dict is None:
+        hole_dict = {}
+
+    if isinstance(node, Literal):
+        if node.char == "":
+            return BasicAutomata.makeEmptyString()
+        return BasicAutomata.makeChar(node.char)
+    elif isinstance(node, Dot):
+        return BasicAutomata.makeAnyChar()
+    elif isinstance(node, Concatenate):
+        children = jpype.JClass("java.util.ArrayList")()
+        for child in node.nodes:
+            children.add(ast_to_automaton(child, hole_dict))
+        return BasicOperations.concatenate(children)
+    elif isinstance(node, Repeat):
+        base = ast_to_automaton(node.node, hole_dict)
+        if node.max is None:
+            return BasicOperations.repeat(base, node.min)
+        return BasicOperations.repeat(base, node.min, node.max)
+    elif isinstance(node, CharacterClass):
+        children = jpype.JClass("java.util.ArrayList")()
+        for item in node.items:
+            children.add(ast_to_automaton(item, hole_dict))
+        if node.negate:
+            return BasicOperations.minus(BasicAutomata.makeAnyChar(), BasicOperations.union(children))
+        return BasicOperations.union(children)
+    elif isinstance(node, Range):
+        return BasicAutomata.makeCharRange(node.start, node.end)
+    elif isinstance(node, Intersection):
+        return BasicOperations.intersection(ast_to_automaton(node.left, hole_dict), ast_to_automaton(node.right, hole_dict))
+    elif isinstance(node, Complement):
+        return BasicOperations.minus(BasicAutomata.makeAnyString(), ast_to_automaton(node.node, hole_dict))
+    elif isinstance(node, Union):
+        return BasicOperations.union(ast_to_automaton(node.left, hole_dict), ast_to_automaton(node.right, hole_dict))
+    elif isinstance(node, StartAnchor):
+        return BasicAutomata.makeEmptyString()
+    elif isinstance(node, EndAnchor):
+        return BasicAutomata.makeEmptyString()
+    elif isinstance(node, Hole):
+        if node.name in hole_dict:
+            return hole_dict[node.name]
+        else:
+            raise ValueError(f"Hole '{node.name}' not provided in hole_dict")
+    elif isinstance(node, PosixClass):
+        name = node.name
+        if name == "upper":
+            return RegExp("[A-Z]").toAutomaton()
+        elif name == "lower":
+            return RegExp("[a-z]").toAutomaton()
+        elif name == "alpha":
+            return RegExp("[A-Za-z]").toAutomaton()
+        elif name == "digit":
+            return RegExp("[0-9]").toAutomaton()
+        elif name == "xdigit":
+            return RegExp("[0-9A-Fa-f]").toAutomaton()
+        elif name == "alnum":
+            return RegExp("[A-Za-z0-9]").toAutomaton()
+        elif name == "punct":
+            return RegExp("[!-/:-@[-`{-~]").toAutomaton()
+        elif name == "blank":
+            return RegExp("[ \t]").toAutomaton()
+        elif name == "space":
+            return RegExp("[ \t\n\r\v\f]").toAutomaton()
+        elif name == "cntrl":
+            return BasicOperations.union(BasicAutomata.makeCharRange(chr(0), chr(31)), BasicAutomata.makeChar(chr(127)))
+        elif name == "graph" or name == "print":
+            return BasicAutomata.makeCharRange(chr(33), chr(126))
+        else:
+            raise ValueError(f"Unknown POSIX character class: {name}")
+    else:
+        raise ValueError(f"Unknown node type: {node}")
+
+def ast_to_automaton(node: Node, hole_dict: Optional[dict[str, Automaton]] = None) -> Automaton:
+    automaton = _ast_to_automaton(node, hole_dict)
+    automaton.setDeterministic(False)
+    automaton.removeDeadTransitions()
+    automaton.minimize()
+    return automaton
+
 if __name__ == "__main__":
-    pattern = input("Enter regex: ")
-    mode = input("Enter mode (basic, extended, compat): ").strip()
+    pattern = "~(.*{{a}}.*)&(a{1,3}{{b}}[ab-e[:digit:]]{3,10})"
+    mode = "compat"
+    hole_dict = {
+        "a": RegExp("aa").toAutomaton(),
+        "b": RegExp("b").toAutomaton(),
+    }
     if mode == "":
         mode = "compat"
     parser = RegexParser(pattern, mode=mode)
-    try:
-        ast = parser.parse()
-        print("Generated AST:")
-        print(ast)
-        ext_regex = ast_to_regex(ast)
-        print("\nTranslated Regex:")
-        print(ext_regex)
-        z3_regex = ast_to_z3(ast)
-        print("\nZ3 Regex:")
-        print(z3_regex)
-    except ValueError as e:
-        print("Parsing error:", e)
+    ast = parser.parse()
+    print("Generated AST:")
+    print(ast)
+    ext_regex = ast_to_regex(ast)
+    print("\nTranslated Regex:")
+    print(ext_regex)
+    automaton = ast_to_automaton(ast, hole_dict)
+    print("\nAutomaton:")
+    print(automaton)
+    shortest = automaton.getShortestExample(True)
+    print(f"\nShortest Example: {shortest!r}")
+    print(ord(shortest[0]))
+    jpype.shutdownJVM()
