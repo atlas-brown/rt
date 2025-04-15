9f57fcbd1d634c846c421925bbf281dd5292ad4e
Zekai Li <zekai.li.0028@gmail.com>
2025-04-15 12:06:18 -0400

    add github commits collection

---
+++ full_benchmark/github_repos_commits/collected/pre_commit/nvm.sh_nvm_cb87c31.sh
@@ -0,0 +1,74 @@
+# Node Version Manager
+# Implemented as a bash function
+# To use source this file from your bash profile
+#
+# Implemented by Tim Caswell <tim@creationix.com>
+# with much bash help from Matthew Ranney
+
+NVM_DIR=$HOME/.nvm
+
+nvm()
+{
+  START=`pwd`
+  if [ $# -lt 1 ]; then
+    nvm help
+    return
+  fi
+  case $1 in
+    "help" )
+      echo
+      echo "Node Version Manager"
+      echo "Usage:"
+      echo "    nvm install version"
+      echo "    nvm use version"
+      echo "    nvm list"
+      echo
+    ;;
+    "install" )
+      if [ $# -lt 2 ]; then
+        nvm help
+        return;
+      fi
+      echo $START
+      mkdir -p "$NVM_DIR/src" && \
+      cd "$NVM_DIR/src" && \
+      wget "http://nodejs.org/dist/node-$2.tar.gz" -N && \
+      tar -xzf "node-$2.tar.gz" && \
+      cd "node-$2" && \
+      ./configure --prefix="$NVM_DIR/$2" && \
+      make && \
+      make install && \
+      nvm use $2
+      cd $START
+    ;;
+    "use" )
+      if [ $# -lt 2 ]; then
+        nvm help
+        return;
+      fi
+      # TODO: Remove old nvm paths before adding this one
+      PATH="$NVM_DIR/$2/bin:$PATH"
+      echo "Now using node $2"
+    ;;
+    "list" )
+      # TODO: put a star by the current active one if possible
+
+################################################################################
+# Commit message: 
+# Commit URL: https://github.com/nvm-sh/nvm/commit/cb87c313a9eaa2a9b7301aa0abaed0dc9d93cd01
+# Category: 
+# Notes: 
+# Changed content:
+# - ls "$NVM_DIR" | grep -v src
+# + ls "$NVM_DIR" | grep -v src | grep -v nvm.sh
+################################################################################
+
+# @output "~(.*(src|nvm\.sh).*)"
+# stream enable
+      ls "$NVM_DIR" | grep -v src
+    ;;
+    * )
+      nvm help
+    ;;
+  esac
+}
---
+++ full_benchmark/github_repos_commits/collected/pre_commit/plugins_mix-fast_mix-fast.plugin.zsh_mix-fast.plugin_bf87e99.zsh
@@ -0,0 +1,42 @@
+_mix_refresh () {
+  if [ -f .mix_tasks ]; then
+    rm .mix_tasks
+  fi
+  echo "Generating .mix_tasks..." > /dev/stderr
+  _mix_generate
+  cat .mix_tasks
+}
+
+_mix_does_task_list_need_generating () {
+  [ ! -f .mix_tasks ];
+}
+
+_mix_generate () {
+################################################################################
+# Commit message: Fix mix-fast plugin (#6708)  The mix command for listing all available actions has been changed from
+# `mix --help` to `mix help`.
+# Commit URL: https://github.com/ohmyzsh/ohmyzsh/commit/bf87e99a14320885c506c6b07c9ead8825ee53b9
+# Category: 
+# Notes: 
+# Changed content:
+# - mix --help | grep -v 'iex -S' | tail -n +2 | cut -d " " -f 2 > .mix_tasks
+# + mix help | grep -v 'iex -S' | tail -n +2 | cut -d " " -f 2 > .mix_tasks
+################################################################################
+# https://en.wikipedia.org/wiki/Mix_(build_tool)
+# if "mix" was properly modeled this bug would have been caught without annotations
+# stream enable
+  mix --help | grep -v 'iex -S' | tail -n +2 | cut -d " " -f 2 > .mix_tasks
+}
+
+_mix () {
+  if [ -f mix.exs ]; then
+    if _mix_does_task_list_need_generating; then
+      echo "\nGenerating .mix_tasks..." > /dev/stderr
+      _mix_generate
+    fi
+    compadd `cat .mix_tasks`
+  fi
+}
+
+compdef _mix mix
+alias mix_refresh='_mix_refresh'
