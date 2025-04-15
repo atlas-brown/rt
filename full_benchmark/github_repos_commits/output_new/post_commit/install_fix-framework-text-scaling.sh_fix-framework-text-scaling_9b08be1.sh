1d468bdc45ee235dd29e55f4f87f5835333f68a9
George Kapetanakis <george.kapetanakis.gk@gmail.com>
2025-04-15 16:44:42 +0300

    Pushed 12 new annotated benchmarks

---
+++ full_benchmark/github_repos_commits/output_new/post_commit/install_fix-framework-text-scaling.sh_fix-framework-text-scaling_9b08be1.sh
@@ -0,0 +1,28 @@
+################################################################################
+# Commit message: Fix match and condition
+# Commit URL: https://github.com/basecamp/omakub/commit/9b08be176e6a2ed77430825eac715047acfffd52
+# Category: 
+# Notes: 
+# Changed content:
+# - COMPUTER_MAKER=$(sudo dmidecode -t system | grep 'Manufacturer' | awk '{print $2}')
+# + COMPUTER_MAKER=$(sudo dmidecode -t system | grep 'Manufacturer:' | awk '{print $2}')
+################################################################################
+# put stream annotation here
+# stream enable
+COMPUTER_MAKER=$(sudo dmidecode -t system | grep 'Manufacturer:' | awk '{print $2}')
+SCREEN_RESOLUTION=$(xrandr | grep '*+' | awk '{print $1}')
+
+################################################################################
+# Commit message: Fix match and condition
+# Commit URL: https://github.com/basecamp/omakub/commit/9b08be176e6a2ed77430825eac715047acfffd52
+# Category: 
+# Notes: 
+# Changed content:
+# - if [ "$COMPUTER_MAKER" == "Framework" && "$SCREEN_RESOLUTION" == "2256x1504" ]; then
+# + if [ "$COMPUTER_MAKER" == "Framework" ] && [ "$SCREEN_RESOLUTION" == "2256x1504" ]; then
+################################################################################
+# put stream annotation here
+# stream enable
+if [ "$COMPUTER_MAKER" == "Framework" ] && [ "$SCREEN_RESOLUTION" == "2256x1504" ]; then
+	gsettings set org.gnome.desktop.interface text-scaling-factor 0.8
+fi
