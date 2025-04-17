db69cb82c06757137153e58ad55b8f700a553e22
Zekai Li <zekai.li.0028@gmail.com>
2025-04-17 12:45:18 -0400

    update the results (final)

--- full_benchmark/pash_benchmark/benchmarks/unix50/scripts/20.sh
+++ full_benchmark/pash_benchmark/benchmarks/unix50/scripts/20.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # 8.3: find names of the four people most involved with unix
-# @file "$1": "[A-Za-z,\\.]+ (\([A-Za-z,\\. ]+\) )*"
+# @file "$1": "[A-Za-z,\\. ]+ (\([A-Za-z,\\. ]+\) )*|[0-9]+"
 # @output "[A-Za-z,\\. ]+"
 cat $1 | grep '(' | cut -d '(' -f 2 | cut -d ')' -f 1 | head -n 1
