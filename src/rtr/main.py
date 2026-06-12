feaa302864b20b4bff834cab05e60822a2544e49
George Kapetanakis <github@kapetanak.is>
2026-06-12 12:01:16 -0400

    Add rtr, a tool that resolves command invocation types, as a second package * First iteration of the tool, still plenty to do

---
+++ src/rtr/main.py
@@ -0,0 +1,67 @@
+import argparse
+
+from stream.parser.shell_parser_util import annot_parser_wrapper as parse_invocation
+from stream.regular_type import RegularType
+from stream.signature_loader import SignatureLoader
+
+
+def cli_main():
+    parser = argparse.ArgumentParser(description="Regular type resolution")
+    parser.add_argument(
+        "command",
+        type=str,
+        help="The command to resolve the regular type for",
+    )
+    parser.add_argument(
+        "args",
+        nargs=argparse.REMAINDER,
+        type=str,
+        help="The invocation args to resolve the regular type for",
+    )
+    parser.add_argument(
+        "-i",
+        "--input-type",
+        type=str,
+        default=None,
+        help="The regular type of the supposed input to the invocation",
+    )
+    args = parser.parse_args()
+    input_type = None
+    if args.input_type is not None:
+        input_type = RegularType(args.input_type)
+    main(args.command, args.args, input_type)
+
+
+def main(command: str, args: list[str], prev_out_type: RegularType | None):
+    invocation = parse_invocation([command, *args])
+    loader = SignatureLoader.get_instance()
+    signature = loader.load_signature(invocation.cmd_name)
+
+    # Determine input type
+    if prev_out_type is None:
+        in_type, _ = signature.determine_input_type(invocation, [], [], {})
+        in_pattern = in_type.pattern
+    else:
+        in_pattern = prev_out_type.pattern
+    assert isinstance(in_pattern, str), "nooooo"
+
+    # Determine output type
+    if prev_out_type is None:
+        prev_out_type = RegularType()
+
+    out_type = signature.determine_output_type(prev_out_type, invocation, [], {})
+    if isinstance(out_type, RegularType):
+        out_pattern = out_type.pattern
+    else:
+        out_pattern = out_type.output_type.pattern
+    assert isinstance(out_pattern, str), "nooooo"
+
+    print("Invocation:")
+    print(" ".join((command, *args)))
+    print()
+    print("Type:")
+    print(in_pattern + " -> " + out_pattern)
+
+
+if __name__ == "__main__":
+    cli_main()
