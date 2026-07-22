f7ba6ab001ac5f9b139e25b4d0a10802f2b065ae
AksharNana <akshar.nana.za@gmail.com>
2026-07-22 11:23:27 -0400

    Base ubuntu/fedora/brew packaging scripts

---
+++ packaging/homebrew/rt.rb
@@ -0,0 +1,69 @@
+# TODO before this formula works:
+#   1. Tag a release on the atlas-brown/rt repo and paste its source tarball
+#      URL + sha256 below.
+class RtArtifact < Formula
+  desc "RT: Regular Types for the Streaming Shell -- static checker for shell pipelines"
+  homepage "https://github.com/atlas-brown/rt"
+  url "TODO-source-tarball-url"
+  sha256 "TODO-source-tarball-sha256"
+  version "0.1.0"
+
+  depends_on "openjdk@21"
+  depends_on "python@3.12"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "openssl@3" => :build
+  depends_on "pkg-config" => :build
+  depends_on "rust" => :build
+  depends_on "uv" => :build
+
+  def install
+    system "patch", "-p1", "-i", "packaging/patches/001-portable-paths.patch"
+
+    ENV["CFLAGS"] = "-std=gnu17 -O2"
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["openssl@3"].opt_lib/"pkgconfig"
+
+    python = Formula["python@3.12"].opt_bin/"python3.12"
+    system "uv", "sync", "--locked", "--no-dev", "--python", python
+
+    site_packages = Pathname.glob(".venv/lib/python3.*/site-packages").first
+    raise "uv sync did not produce a site-packages directory" if site_packages.nil?
+
+    (site_packages/"stream.pth").unlink if (site_packages/"stream.pth").exist?
+    rm_rf site_packages/"stream"
+    rm_rf site_packages/"rtr"
+    cp_r "src/stream", site_packages/"stream"
+    cp_r "src/rtr", site_packages/"rtr"
+    Dir.glob("#{site_packages}/**/__pycache__").each { |d| rm_rf d }
+    %w[stream rtr].each do |pkg|
+      unit_tests = site_packages/pkg/"unit_tests"
+      rm_rf unit_tests if unit_tests.exist?
+    end
+
+    libexec.install site_packages => "site-packages"
+    libexec.install "jars"
+
+    java_home = Formula["openjdk@21"].opt_prefix/"libexec/openjdk.jdk/Contents/Home"
+
+    {
+      "rt" => "stream.main",
+      "rtr" => "rtr.main",
+    }.each do |cmd, entry_module|
+      (bin/cmd).write <<~SH
+        #!/bin/sh
+        export PYTHONPATH="#{libexec}/site-packages${PYTHONPATH:+:$PYTHONPATH}"
+        export RT_JARS_DIR="#{libexec}/jars"
+        export JAVA_HOME="#{java_home}"
+        exec "#{python}" -m #{entry_module} "$@"
+      SH
+      (bin/cmd).chmod 0755
+    end
+  end
+
+  test do
+    system bin/"rt", "--help"
+    system bin/"rtr", "--help"
+  end
+end
